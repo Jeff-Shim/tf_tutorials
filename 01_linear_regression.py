b00b095ade1a8827acc0c8c449704aef9467c268
woozzu <woozzu@gmail.com>
2017-05-24 13:57:39 +0900

    initial commit

---
+++ 01_linear_regression.py
@@ -0,0 +1,64 @@
+"""
+Simple linear regression example in TensorFlow
+This program tries to predict the number of thefts from 
+the number of fire in the city of Chicago
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import csv
+
+DATA_FILE = 'data/fire_theft.csv'
+
+# Step 1: read data
+with open(DATA_FILE, 'r') as f:
+    data = []
+    reader = csv.reader(f, delimiter=',')
+    for i, row in enumerate(reader):
+        if i == 0:
+            continue
+        data.append(row)
+    n_samples = len(data)
+    data = np.asarray(data, dtype='float32')
+
+# Step 2: create placeholders for input X (number of fire) and label Y (number of theft)
+X = tf.placeholder(tf.float32, name='X')
+Y = tf.placeholder(tf.float32, name='Y')
+
+# Step 3: create weight and bias, initialized to 0
+w = tf.Variable(0.0, name='weights')
+b = tf.Variable(0.0, name='bias')
+
+# Step 4: build model to predict Y
+Y_predicted = X * w + b
+
+# Step 5: use the square error as the loss function
+loss = tf.reduce_mean(tf.square(Y - Y_predicted, name='loss'))
+
+# Step 6: using gradient descent with learning rate of 0.01 to minimize loss
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+train = optimizer.minimize(loss)
+
+with tf.Session() as sess:
+    # Step 7: initialize the necessary variables, in this case, w and b
+    sess.run(tf.global_variables_initializer())
+
+    # Step 8: train the model
+    for i in range(100):  # train the model 100 times
+        total_loss = 0
+        for x, y in data:
+            # Session runs train_op and fetch values of loss
+            _, l = sess.run([train, loss], feed_dict={X: x, Y: y})
+            total_loss += l
+        print('Epoch {0}: {1}'.format(i, total_loss / n_samples))
+
+    # Step 9: output the values of w and b
+    w_value, b_value = sess.run([w, b])
+
+# plot the results
+X, Y = data.T[0], data.T[1]
+plt.plot(X, Y, 'bo', label='Real data')
+plt.plot(X, X * w_value + b_value, 'r', label='Predicted data')
+plt.legend()
+plt.show()
---
+++ 01_multivar_linear_regression.py
@@ -0,0 +1,64 @@
+"""
+Simple linear regression example in TensorFlow
+This program tries to predict the final test score
+of general psychology based on previous exam scores
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import csv
+
+DATA_FILE = 'data/test_scores.csv'
+
+# Step 1: read data
+with open(DATA_FILE, 'r') as f:
+    data = []
+    reader = csv.reader(f, delimiter=',')
+    for i, row in enumerate(reader):
+        if i == 0:
+            continue
+        data.append(row)
+    n_samples = len(data)
+    data = np.asarray(data, dtype='float32')
+
+# Step 2: create placeholders
+X = tf.placeholder(tf.float32, shape=[None, 3], name='X')
+Y = tf.placeholder(tf.float32, shape=[None, 1], name='Y')
+
+# Step 3: create weight and bias
+W = tf.Variable(tf.random_normal([3, 1]), name='weights')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Step 4: build model to predict Y
+Y_predicted = tf.matmul(X, W) + b
+
+# Step 5: use the square error as the loss function
+loss = tf.reduce_mean(tf.square(Y - Y_predicted, name='loss'))
+
+# Step 6: using gradient descent with learning rate of 0.01 to minimize loss
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=2e-7)
+train = optimizer.minimize(loss)
+
+with tf.Session() as sess:
+    # Step 7: initialize the necessary variables, in this case, w and b
+    sess.run(tf.global_variables_initializer())
+
+    # Step 8: train the model
+    for i in range(100):  # train the model 100 times
+        total_loss = 0
+        for x1, x2, x3, y in data:
+            # Session runs train_op and fetch values of loss
+            _, l = sess.run([train, loss], feed_dict={X: [[x1, x2, x3]], Y: [[y]]})
+            total_loss += l
+        print('Epoch {0}: {1}'.format(i, total_loss / n_samples))
+
+    # Step 9: output the values of w and b
+    W_value, b_value = sess.run([W, b])
+
+# # plot the results
+# X, Y = data.T[0], data.T[1]
+# plt.plot(X, Y, 'bo', label='Real data')
+# plt.plot(X, X * w_value + b_value, 'r', label='Predicted data')
+# plt.legend()
+# plt.show()
